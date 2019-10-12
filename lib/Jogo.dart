73176ecda870e15bf2a9e3a02d506c9b13924324
Daniel Bonfim <daniel.bonfim@cubos.io>
2019-10-12 12:02:16 -0300

    interface inicial

--- lib/Jogo.dart
+++ lib/Jogo.dart
@@ -13,13 +13,47 @@ class _JogoState extends State<Jogo> {
         title: Text("JokenPo"),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           //text
           //imagem
 
           //text resultado
           //linha 3 imagens
-          
+
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha do App",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Image.asset("images/padrao.png"),
+
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha uma opção abaixo",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Image.asset("images/pedra.png", height: 100),
+              Image.asset("images/papel.png", height: 100),
+              Image.asset("images/tesoura.png", height: 100),
+            ],
+          )
+
         ],
       ),
     );
